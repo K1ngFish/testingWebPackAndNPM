84f3e48878a1e89107314bac5089212fb7d27bc1
Denis Novikov <novikov.denis.v@gmail.com>
2023-12-14 00:28:30 +0400

    project for testing frontend features via WebPack and NPM initiated

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/script.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "script_bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index_bundle.html",
+            template: "src/index.html",
+            removeComments: true,
+            // minify: false,
+            // favicon: "./src/assets/favicon.png"
+        }),
+        // new HtmlWebpackPlugin({
+        //     filename: "second_index_bundle.html",
+        //     template: "src/second_index.html",
+        // })
+    ]
+}
